5b3f94eb35a872aac93445c7142978e603f915e6
oyvindsabo <oyvind.sabo@gmail.com>
2018-07-19 23:58:54 +0200

    Add adverbize, toBase, toInfnitive and toPresentTense functions.

--- nlp.js
+++ nlp.js
@@ -1,23 +1,58 @@
-
-/* Turns a noun into an adjective */
-const adjectivize = (noun) => {
-  return noun.slice(0, -1) + 'a'
-}
-
-/* Adds the definite article to a noun */
-/*
- * TODO: Make the function accept all combinations of words which given a formal grammar of the
- * language corresponds to a noun
- */
-const definitize = (noun) => {
-  return 'la ' + noun;
-}
-
-/* Pluralizes a noun or an ajective */
 /**
- * TODO: Don't add 'j' if the word is already in plural
- * If there is a noun and an adjective, bot should be pluralized
+ *  _ __     ___    _   _   _ __    ___ 
+ * | '_ \   / _ \  | | | | | '_ \  / __|
+ * | | | | | (_) | | |_| | | | | | \__ \
+ * |_| |_|  \___/   \__,_| |_| |_| |___/
+ */                                      
+
+/** Turns a noun or an adverb (or an adjective) an into an adjective */
+const adjectivize = (word) => {
+  return word.slice(0, -1) + 'a'
+}
+
+/** Turns a noun or an adjective (or an adverb) into an adverb */
+const adverbize = (word) => {
+	return word.slice(0, -1) + 'e'
+}
+
+/** Adds the definite article to a noun if it's not already there */
+const definitize = (noun) => {
+  return noun.substring(0, 3) === 'la ' ? noun : 'la ' + noun
+}
+
+/**
+ * Pluralizes all nouns or ajectives in a sentence
+ * Currently doesn't handle commas or periods
  */
-const pluralize = (word) => {
-  return word + 'j'
+const pluralize = (words) => {
+  return words.split(' ').map(word => 'oa'.indexOf(word.substr(-1)) !== -1 ? word + 'j' : word).join(' ')
+}
+
+/**
+ *                        _           
+ * __   __   ___   _ __  | |__    ___ 
+ * \ \ / /  / _ \ | '__| | '_ \  / __|
+ *  \ V /  |  __/ | |    | |_) | \__ \
+ *   \_/    \___| |_|    |_.__/  |___/
+ */                                    
+
+/** Stems the verb i.e. finds the base (dictionary form) of the verb */
+const toBase = (verb) => {
+	return verb.substr(-2) === 'as' ? verb.slice(0, -2) : verb.substr(-1) === 'i' ? verb.slice(0, -1) : verb
+}
+
+/**
+ * Converts a verb to infinitive
+ * Currently only accepts base, infinitive and present tense
+ */
+const toInfinitive = (verb) => {
+	return verb.substr(-2) === 'as' ? verb.slice(0,-2) + 'i' : verb
+}
+
+/**
+ * Converts a verb to present tense.
+ * Currently only accepts base, infinitive and present tense
+ */
+const toPresentTense = (verb) => {
+	return verb.substr(-1) === 'i' ? verb.slice(0, -1) + 'as' : verb.substr(-2) === 'as' ? verb : verb + 'as'
 }
