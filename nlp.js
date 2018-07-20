0e8fc0c8e14a35cfe2b202108158992f2bf2e9f4
oyvindsabo <oyvind.sabo@gmail.com>
2018-07-20 16:17:45 +0200

    Converted all functions to true one-liners

--- nlp.js
+++ nlp.js
@@ -2,57 +2,41 @@
 /** NOUNS */
 
 /** Turns a noun or an adverb (or an adjective) into an adjective */
-const adjectivize = (word) => {
-  return word.slice(0, -1) + 'a'
-}
+const adjectivize = word => word.slice(0, -1) + 'a'
 
 /** Turns a noun or an adjective (or an adverb) into an adverb */
-const adverbize = (word) => {
-  return word.slice(0, -1) + 'e'
-}
+const adverbize = word => word.slice(0, -1) + 'e'
 
 /** Adds the definite article to a noun if it's not already there */
-const definitize = (noun) => {
-  return noun.substring(0, 3) === 'la ' ? noun : 'la ' + noun
-}
+const definitize = noun => noun.substring(0, 3) === 'la ' ? noun : 'la ' + noun
 
 /**
  * Pluralizes all nouns or ajectives in a sentence
  * Currently doesn't handle commas or periods
  */
-const pluralize = (words) => {
-  return words.split(' ').map(word => 'oa'.indexOf(word.substr(-1)) !== -1 ? word + 'j' : word).join(' ')
-}
+const pluralize = words => words.split(' ').map(word => 'oa'.indexOf(word.substr(-1)) !== -1 ? word + 'j' : word).join(' ')
 
 /** PRONOUNS */
 
 /** Adds an a to the end of a pronoun if it's not already there */
-const toPossessive = (pronoun) => {
-  return pronoun.substr(-1) === 'a' ? pronoun : pronoun + 'a'
-}
+const toPossessive = pronoun => pronoun.substr(-1) === 'a' ? pronoun : pronoun + 'a'
 
 /** VERBS */
 
 /** Stems the verb i.e. finds the base (dictionary form) of the verb */
-const toBase = (verb) => {
-  return verb.substr(-2) === 'as' ? verb.slice(0, -2) : verb.substr(-1) === 'i' ? verb.slice(0, -1) : verb
-}
+const toBase = (verb) => verb.substr(-2) === 'as' ? verb.slice(0, -2) : verb.substr(-1) === 'i' ? verb.slice(0, -1) : verb
 
 /**
  * Converts a verb to infinitive
  * Currently only accepts base, infinitive and present tense
  */
-const toInfinitive = (verb) => {
-  return verb.substr(-2) === 'as' ? verb.slice(0,-2) + 'i' : verb
-}
+const toInfinitive = verb => verb.substr(-2) === 'as' ? verb.slice(0,-2) + 'i' : verb
 
 /**
  * Converts a verb to present tense.
  * Currently only accepts base, infinitive and present tense
  */
-const toPresentTense = (verb) => {
-  return verb.substr(-1) === 'i' ? verb.slice(0, -1) + 'as' : verb.substr(-2) === 'as' ? verb : verb + 'as'
-}
+const toPresentTense = verb => verb.substr(-1) === 'i' ? verb.slice(0, -1) + 'as' : verb.substr(-2) === 'as' ? verb : verb + 'as'
 
 /** NEGATION */
 
@@ -60,6 +44,4 @@ const toPresentTense = (verb) => {
  * Adds ne before the first present tense verb in the sentence if any
  * Currently doesn't handle commas or periods
  */
-const negate = (sentence) => {
-  return sentence.split(' ').map((word, index) => word.substr(-2) === 'as' ? 'ne ' + word : word).join(' ')
-}
+const negate = sentence => sentence.split(' ').map((word, index) => word.substr(-2) === 'as' ? 'ne ' + word : word).join(' ')
